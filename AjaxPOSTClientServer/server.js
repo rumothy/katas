614aa61b2399dae254809dadf6620b9e5026f42a
rumothy <rumothy@yahoo.com>
2020-01-09 08:16:25 -0500

    Better naming less typing

--- AjaxPOSTClientServer/server.js
+++ AjaxPOSTClientServer/server.js
@@ -6,16 +6,14 @@ const fs = require("fs");
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-let items = [
-    {id: 1, name: "rbd"},
-];
+let items = [ {id: 1, name: "shiny item"} ];
 
 app.get("/", function(req, res){
     sendItemsHtml(res);
 });
 
 app.post("/api/items", function(req, res){
-    let item = { id: items.length + 1, name: req.body.userInput };
+    let item = { id: items.length + 1, name: req.body.itemName };
     items.push(item);
     sendItemsHtml(res);
 });
@@ -24,12 +22,12 @@ app.post("/api/items", function(req, res){
 function sendItemsHtml(res){
     fs.readFile(`${__dirname}/index.html`, "utf-8", function(err, data){
         if (err) throw err;
-        res.end(data.replace("{{{items}}}", createItemsHtml()));
+        res.end(data.replace("{{{_items}}}", createItemsHtml()));
     });
 }
 
 function createItemsHtml(){
-    let itemsHtmlArry = items.map(x => `<li>${x.id}. ${x.name}</li>`);
+    let itemsHtmlArry = items.map(item => `<li>${item.id}. ${item.name}</li>`);
     return itemsHtmlArry.join('');
 }
 
